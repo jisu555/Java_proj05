73dabaaa1d21be9a47ee0313818bfd860a23137f
Jisu Kim <superformance9@gmail.com>
2022-11-10 18:24:01 +0900

    11월 10일

---
+++ src/chapter02/ex08/Ex01.java
@@ -0,0 +1,19 @@
+package chapter02.ex08;
+
+import java.util.Scanner;
+
+public class Ex01 {
+
+	public static void main(String[] args) {
+		// <문제> 스캐너를 등록하고 , 
+		// 어머님이름, 아버님이름, 형제이름, 자신이름     sc.next();
+		// 어머님나이, 아버님나이, 형제나이, 자신나이     sc.nextInt();
+		
+		//콘솔에서 인풋받은 이름은 모두 출력
+		//나이의 합계와 평균을 출력
+
+	
+		
+	}
+
+}
